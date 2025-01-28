bff9c05e90705e80c3f76e2a81641cf92ccca7b4
Nidhi Rajvir <rajvirnidhi@gmail.com>
2025-01-28 18:27:53 +0530

    Adding display and append methods

--- 1.LinkedLists/LinkedList.java
+++ 1.LinkedLists/LinkedList.java
@@ -8,10 +8,36 @@ public class LinkedList
     {
         length=0;
     }
+
     public LinkedList(int value)
     {
         Node newNode = new Node(value);
         head=tail=newNode;
         length=1;
     }
+
+    public void display()
+    {
+        Node temp=head;
+        while (temp!=null) {
+            System.out.println(temp.value);
+            temp=temp.next;
+        }
+    }
+
+    public void append(int value)
+    {
+        Node newNode = new Node(value);
+        if(length==0)
+        {
+            head=tail=newNode;
+        }
+        else
+        {
+            tail.next=newNode;
+            tail=newNode;
+        }
+        length++;
+    }
+    
 }
--- 1.LinkedLists/Main.java
+++ 1.LinkedLists/Main.java
@@ -5,5 +5,27 @@ public class Main {
         Scanner s = new Scanner(System.in);
         LinkedList myLinkedList = new LinkedList();
         System.out.println("Welcome to linkedLists");
+        int option;
+        do{
+            System.out.println("Select the operation");
+            System.out.println("1. Display");
+            System.out.println("2. Append");
+            System.out.println("0. Exit");
+            option=s.nextInt();
+            if(option==0)
+                break;
+            if(option==1)
+            {
+                System.out.println("Displaying list");
+                myLinkedList.display();
+            }
+            else if(option==2)
+            {
+                System.out.println("Enter value to append");
+                int val=s.nextInt();
+                myLinkedList.append(val);
+            }
+        }while(option!=0);
+
     }
 }
